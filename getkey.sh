abf69509ea901980f58c290e1ff58fb129e3cc30
Marco Crotta <marco.crotta@food-chain.it>
2019-04-09 12:43:29 +0200

    test

---
+++ getkey.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PWD=$(pwd)
+DIR="/home/quadrans/.quadrans/keystore"
+PAS=$(cat "/home/quadrans/password.txt")
+
+RESET='\e[0m'
+BGNOCOLOR='\e[49m'
+BGBLUE='\e[44m'
+BGMAGENTA='\e[45m'
+BGCYAN='\e[46m'
+WHITE='\e[97m'
+BOLD='\e[1m'
+
+echo -e $WHITE$BOLD
+echo -e    $BGBLUE" Q U A D R A N S                                                               "$BGNOCOLOR
+echo -e $BGMAGENTA"   Foundation                                                                  "$BGNOCOLOR
+echo -e    $BGCYAN"                                                        Private key retriever  "$BGNOCOLOR
+echo -e $RESET
+
+cd $DIR
+for W in $(ls UTC*)
+do
+  K=$($PWD/gk $W $PAS)
+  if [[ "$K" != "" ]]; then
+    echo;echo "----------------------------------------------------"
+    echo; echo -e $WHITE$BOLD* "Wallet file:";echo -e $RESET
+    cat $W | python -m json.tool
+    echo;echo; echo -e $WHITE$BOLD"* Private key:";echo -e $RESET
+    echo $K
+    echo;echo "----------------------------------------------------"
+  fi
+done
