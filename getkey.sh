4771fc6bcd3884caba83806a3b3c0f8b3fec2dc3
Marco Crotta <marco.crotta@food-chain.it>
2019-04-10 17:18:05 +0200

    more automation

--- getkey.sh
+++ getkey.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-BSD=$(pwd)
+###### Constants ######
+
+TMP=/tmp
 DIR="/home/quadrans/.quadrans/keystore"
+PROG="https://github.com/BlockChainCaffe/getkey/raw/master/gk"
 PAS=$(cat "/home/quadrans/password.txt")
 
 RESET='\e[0m'
@@ -12,22 +15,58 @@ BGCYAN='\e[46m'
 WHITE='\e[97m'
 BOLD='\e[1m'
 
-echo -e $WHITE$BOLD
-echo -e    $BGBLUE" Q U A D R A N S                                                               "$BGNOCOLOR
-echo -e $BGMAGENTA"   Foundation                                                                  "$BGNOCOLOR
-echo -e    $BGCYAN"                                                        Private key retriever  "$BGNOCOLOR
-echo -e $RESET
+###### Functions ######
+
+function required {
+  which $1 2>&1 >> /dev/null
+  if [[ "$?" == 1 ]]; then
+    apt-get istall -y $1
+  fi
+}
+
+function header {
+  clear
+  echo -e $WHITE$BOLD
+  echo -e    $BGBLUE" Q U A D R A N S                                                               "$BGNOCOLOR
+  echo -e $BGMAGENTA"   Foundation                                                                  "$BGNOCOLOR
+  echo -e    $BGCYAN"                                                        Private key retriever  "$BGNOCOLOR
+  echo -e $RESET
+}
+
+###### Requirements ######
+
+IAM=$(id -u)
+if [[ "$IAM" != "0" ]]; then
+  echo "This program needs to be run as user root"
+  exit
+fi
+
+header
+echo; echo "Installing missing dependencies... "
+required zip
+required python
+required wget
+
+
+header
+echo; echo "Downloading needed Software... "
+#wget -q $PROG -O $TMP/gk
+chmod +x $TMP/gk
 
 cd $DIR
+header
+
 for W in $(ls UTC*)
 do
-  K=$($BSD/gk $W $PAS)
+  K=$($TMP/gk $W $PAS)
   if [[ "$K" != "" ]]; then
-    echo;echo "----------------------------------------------------"
+    echo;echo "-------------------------------------------------------------------------------"
     echo; echo -e $WHITE$BOLD* "Wallet file:";echo -e $RESET
     cat $W | python -m json.tool
     echo;echo; echo -e $WHITE$BOLD"* Private key:";echo -e $RESET
-    echo $K
-    echo;echo "----------------------------------------------------"
+    echo -e "\t"$K
+    echo;echo "-------------------------------------------------------------------------------"
   fi
 done
+
+#rm -Rf $TMP/gk
